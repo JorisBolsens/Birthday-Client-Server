67deef0da78256a7c4a3603bab535b9f5fed0aa6
Joris Bolsens <joris@jojolabs.com>
2019-05-22 21:46:33 -0700

    add comments

--- client/client.js
+++ client/client.js
@@ -4,15 +4,20 @@ const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
 });
+
+//These months have 31 days
 const months31 = [1,3,5,7,8,10,12];
 
-
+//Basic error checking
 const checkRequest = function(firstName, lastName, month, day) {
+    //make sure everything is defined and correct type
     if (firstName === undefined || lastName === undefined || isNaN(day) || isNaN(month))
         console.error("Must include FirstName, LastName, BirthDay, and BirthMonth\n" +
             "BirthMonth and BirthDay must be numeric");
+    //make sure month is in range
     else if (month < 1 || month > 12)
         console.error("Birth month must be between 1 (January) and 12 (December)");
+    //make sure day is in range
     else if (day < 1 || (month === 2 && day > 29) ||
         (months31.includes(Number(month)) && day > 31) ||
         (month !== 2 && !months31.includes(Number(month)) && day > 30))
@@ -21,6 +26,7 @@ const checkRequest = function(firstName, lastName, month, day) {
         sendRequest(firstName, lastName, month, day)
 };
 
+//Send request to the server
 const sendRequest = function(firstName, lastName, month, day){
     request.post('http://localhost:8765/', {
         json : {
@@ -38,19 +44,23 @@ const sendRequest = function(firstName, lastName, month, day){
     });
 };
 
+//Prompt user for input
 const promptUser = function () {
     console.log("Please enter a firstName lastName birthMonth birthDay (or 0 or Ctrl-C to exit)");
 };
 
+//check number of args
+//incorrect number of args
 if (process.argv.length !== 2 && process.argv.length !== 6){
     console.error("must use either no arguments, or provide firstName lastName birthMonth birthDay");
     return -1;
-} else if(process.argv.length === 6){
+} else if(process.argv.length === 6){ //name and date provided as arguments
     checkRequest(process.argv[2], process.argv[3], Number(process.argv[4]), Number(process.argv[5]));
     setTimeout(promptUser, 100)
 } else
     promptUser();
 
+//handle new input
 rl.on('line', (line) => {
     if(line.trim()==="0") {
         console.log("Goodbye!");
--- server/server.js
+++ server/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8765;
-const months31 = [1,3,5,7,8,10,12];
 
+const months31 = [1,3,5,7,8,10,12]; //these months have 31 days
+
+//This will compare two dates and return the difference between them in the year.
 const compareDates = function (today, birthDay) {
     let days = Math.ceil((birthDay - today) / (1000 * 86400));
 
     return days > 0 ? days : 365 + days;
 };
 
+//Process a GET request and set the body field
 const getHandler = function (req, res, next) {
     req.body = {
         'BirthMonth': Number(req.params.month),
@@ -20,7 +23,10 @@ const getHandler = function (req, res, next) {
 
     next();
 };
+
+//Some Basic error handling
 const errorHandler = function(req, res, next) {
+    //Make sure that each field is defined and the proper type
     if(req.body === undefined || req.body.BirthDay === undefined || req.body.BirthMonth === undefined ||
         req.body.FirstName === undefined || req.body.LastName === undefined || isNaN(req.body.BirthDay) ||
         isNaN(req.body.BirthMonth))
@@ -33,22 +39,31 @@ const errorHandler = function(req, res, next) {
                 "  \"LastName\": \"Bolsens\"\n" +
             "}"
         );
+    //Make sure that the month is within range
     else if(req.body.BirthMonth < 1 || req.body.BirthMonth > 12)
         res.status(400).send("Birth month must be between 1 (January) and 12 (December)");
+    //Make sure that the day is within range
     else if(req.body.BirthDay < 1 || (req.body.BirthMonth === 2 && req.body.BirthDay > 29) ||
         (months31.includes(req.body.BirthMonth) && req.body.BirthDay > 31) ||
         (req.body.BirthMonth !== 2 && !months31.includes(req.body.BirthMonth) && req.body.BirthDay > 30))
         res.status(400).send("Birth day cannot be less than 1 or greater than days per month");
     else
-        next();
+        next(); //move to next handler
 };
+
+//Final handler
 const responseHandler = function(req, res) {
+    //get todays date
     const today = new Date();
+    //get the birthdate
     const birthday = new Date(today.getFullYear(), req.body.BirthMonth - 1, req.body.BirthDay);
 
+    //base message
     let message = "Hello " + req.body.FirstName + ". ";
+    //check if today is their birthday
     if(today.getDay() === birthday.getDay() && today.getMonth() === birthday.getMonth())
         message += "Happy Birthday!";
+    //if it is not their birthday
     else
         message += "You have " + compareDates(today, birthday) + " days until your birthday!";
 
@@ -56,7 +71,8 @@ const responseHandler = function(req, res) {
 };
 
 app.use(bodyParser.json());
-app.get('/:first(\\w+)/:last(\\w+)/:month(\\d{2})/:day(\\d{2})', [getHandler, errorHandler, responseHandler]);
+//use regex to validate GET params
+app.get('/:first(\\w+)/:last(\\w+)/:month(\\d{1,2})/:day(\\d{1,2})', [getHandler, errorHandler, responseHandler]);
 app.post('/', [errorHandler, responseHandler]);
 
 app.listen(port, ()=> console.log("Listening!"));
